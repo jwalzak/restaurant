26ba512cfcb0fb0ff3304dfc667dccbf6bfe29ab
Jason Walzak <walzak@gmail.com>
2018-02-19 15:48:49 -0500

    added google maps to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react'
+import config from './config.js'
 
 class Footer extends Component {
+  static defaultProps = {
+    center: {lat: 42.66571, lng: -81.2134618},
+    zoom: 17,
+    size: {width: 640, height: 380},
+  }
+  
   render() {
     return (
       <div className="footer">
         <p>Find us next to the bridge in Port Stanley</p>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: config.API_KEY }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        />
       </div>
     )
   }
