32d779769aa1b5e476a520783508d1fe4fe816fa
Jason Walzak <walzak@gmail.com>
2018-02-19 23:18:49 -0500

    Footer.js Added new parameters to the GoogleMapReact component Added a marker component Import Marker component changed zoom level to 18
    Marker.js
    Created the file
    Made an empty div
    
    App.css
    Changed view height of menu component
    Changed view height of footer component
    Tried to change the width of the map component
    Styled the marker component to be a black box -- This is visible on the map.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react'
+
+// Imports I made
+import Marker from './Marker'
 import config from './config.js'
 
 class Footer extends Component {
   static defaultProps = {
-    center: {lat: 42.66571, lng: -81.2134618},
-    zoom: 17,
-    size: {width: 640, height: 380},
+    center: { lat: 42.66571, lng: -81.2134618 },
+    zoom: 18,
+    size: { width: 300, height: 100 },
   }
-  
+
   render() {
     return (
       <div className="footer">
-        <p>Find us next to the bridge in Port Stanley</p>
         <GoogleMapReact
           bootstrapURLKeys={{ key: config.API_KEY }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
-        />
+          draggable={false}
+          title={'Find us near the lift bridge in Port Stanley Ontario'}
+        >
+          <Marker
+            lat={42.66571}
+            lng={-81.2134618}
+            text={'This is where we are'}
+          />
+        </GoogleMapReact>
       </div>
     )
   }
